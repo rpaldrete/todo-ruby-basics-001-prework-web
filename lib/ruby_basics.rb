b86eca97d65a247395fdeab78688d6517eca10f9
rpaldrete <rpaldrete@gmail.com>
2015-11-27 21:07:15 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,49 @@
-def division(num1, num2)
+def division(num1,num2)
+  num1/num2
  
 end
+puts division(42,7)
 
 def assign_variable(value)
+  value = "Bob"
+end
+puts assign_variable("value")
+
+def argue(phrase) 
+  phrase = "I'm right and you are wrong!"
   
 end
+puts argue("phrase")
 
-def argue
-  
+
+def greeting(greeting,name)
+greeting = "Hi there, "
+name = "Bobby!"
+puts "#{greeting}#{name}"
 end
+puts greeting("greeting", "name")
 
-def greeting
-
-end
 
 def return_a_value
+   "Nice"
 
 end
+return_a_value()
 
 def last_evaluated_value
+   best_hairstyle = "Guy Fieri"
+   she_expert = "expert"
 
 end
+last_evaluated_value()
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+  puts "#{pizza}"
+  return "#{pizza}"
+  
 end
+pizza_party()
+puts pizza_party("pepperoni")
+
+
+
